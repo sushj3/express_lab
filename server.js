ccc57d65c196791605bbfb32d9158e59ad5f0c2e
Sushil Jacob <sushjacob3@gmail.com>
2025-11-10 17:48:10 -0500

    Added a new Router

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require("express");
+const express = 
+require("express");
+const userRouter = 
+require('./routes/users')
 const app = express(); //Calling this function sets up a server
 app.set('view engine', 'ejs');
+app.use('/users', userRouter);
+
+
 app.get('/', (req, res)=>{
     console.log("Here");
     res.render("index", {user: "Sushil!"});
