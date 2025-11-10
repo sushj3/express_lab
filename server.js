4f98bd9a75bf3d6b2c87037de5c314ecee8d5ba8
Sushil Jacob <sushjacob3@gmail.com>
2025-11-10 16:45:33 -0500

    Added EJS and created first HTML page

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express(); //Calling this function sets up a server
-
+app.set('view engine', 'ejs');
 app.get('/', (req, res)=>{
     console.log("Here");
-    res.send("Hello World!");
+    res.render("index");
     
 }); // this function will run when someone goes to the root folder
 
