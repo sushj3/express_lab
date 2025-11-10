6345bf712126e8798b95b5a9e5653d9ce38679b5
Sushil Jacob <sushjacob3@gmail.com>
2025-11-10 17:33:20 -0500

    Added users routes

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const app = express(); //Calling this function sets up a server
 app.set('view engine', 'ejs');
 app.get('/', (req, res)=>{
     console.log("Here");
-    res.render("index");
+    res.render("index", {user: "Sushil!"});
     
 }); // this function will run when someone goes to the root folder
+app.get('/users', (req, res)=>{
+    res.send('User List');
+});
+app.get('/users/new', (req, res)=>{
+    res.send('New User Form');
+});
+
+
 
 app.listen(3030);
 
