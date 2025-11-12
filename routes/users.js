1167a87875bcc8606abdff39aee665108d19e03e
Sushil Jacob <sushjacob3@gmail.com>
2025-11-12 13:47:05 -0500

    Made id route

--- routes/users.js
+++ routes/users.js
@@ -10,5 +10,17 @@ router.get('/', (req, res)=>{
 router.get('/new', (req, res)=>{
     res.send('New User Form');
 });
+router.post('/', (req, res)=>{
 
+});
+//router.get(':/id', (req, res)=>{
+   /// res.send(`Getting User Data: ${req.params.id}`);
+//});
+router.route("/:id").get('/:id', (req, res)=>{
+    res.send(`Getting User Data: ${req.params.id}`);
+}).delete((req, res)=>{
+    res.send(`Deleting user with id: ${req.params.id}`);
+}).put((req, res)=>{
+    res.send(`Updating user with id: ${req.params.id}`);
+});
 module.exports = router;
