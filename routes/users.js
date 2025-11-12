b8154c570f73004fd7bbd0b0c4a7fed22d3f48bc
Sushil Jacob <sushjacob3@gmail.com>
2025-11-12 14:00:10 -0500

    Added id param function

--- routes/users.js
+++ routes/users.js
@@ -16,11 +16,16 @@ router.post('/', (req, res)=>{
 //router.get(':/id', (req, res)=>{
    /// res.send(`Getting User Data: ${req.params.id}`);
 //});
-router.route("/:id").get('/:id', (req, res)=>{
+router.route("/:id").get((req, res)=>{
     res.send(`Getting User Data: ${req.params.id}`);
 }).delete((req, res)=>{
     res.send(`Deleting user with id: ${req.params.id}`);
 }).put((req, res)=>{
     res.send(`Updating user with id: ${req.params.id}`);
+});
+
+router.param("id", (req, res, next, id)=>{
+    console.log(`Accessing user #${id}`);
+    next();
 });
 module.exports = router;
