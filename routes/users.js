f640fc7125604b948d5f2c7eed054e2720d7d60c
Sushil Jacob <sushjacob3@gmail.com>
2025-11-17 14:14:20 -0500

    Made new ejs files and form

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,28 @@ const router =
 express.Router();
 
 router.get('/', (req, res)=>{
-    res.send('User List');
+    res.render('users/list', {users:users});
 });
 router.get('/new', (req, res)=>{
     res.send('New User Form');
 });
 router.post('/', (req, res)=>{
-
+    res.send("User Created!");
+    const name = req.body.firstName;
+    const isValid = firstName !=="";
+    if(isValid)
+    {
+        console.log(`Adding User: ${name}`);
+        users.push({name: firstName});
+        console.log(`New set of Users ${users}`);
+        res.send("User Created!");
+    }
+    else{
+        console.log("Error adding user!");
+        res.render("users/new", {firstName: firstName});
+        
+    }
+    
 });
 //router.get(':/id', (req, res)=>{
    /// res.send(`Getting User Data: ${req.params.id}`);
@@ -24,6 +39,9 @@ router.route("/:id").get((req, res)=>{
     res.send(`Updating user with id: ${req.params.id}`);
 });
 const users = [{name:"Sushil"},{name:"George"}];
+
+
+
 router.param("id", (req, res, next, id)=>{
     console.log(`Accessing user #${id}`);
     next();
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = 
-require("express");
-const userRouter = 
-require('./routes/users')
+const express = require("express");
+const userRouter = require('./routes/users')
 const app = express(); //Calling this function sets up a server
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+//app.use(logger);
 app.use('/users', userRouter);
+app.use('/posts', postRouter);
 
 
 app.get('/', (req, res)=>{
@@ -18,8 +20,16 @@ app.get('/users', (req, res)=>{
 app.get('/users/new', (req, res)=>{
     res.send('New User Form');
 });
-
+app.get('/new', (req, res)=>{
+    res.render('users/new', {firstName: "Please enter your name"});
+});
 
 
 app.listen(3030);
+
+function logger(req, res, next){
+    console.log(`Page Accessed: ${req.originalUrl}`);
+    next();
+    
+}
 
